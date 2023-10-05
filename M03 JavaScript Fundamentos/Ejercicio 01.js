e25a1835ede1230ba29ee6d5ad7a5defcd428064
JorgeFichera <estudiofichera@gmail.com>
2023-10-05 00:42:23 -0300

    AGREGO TAREA MODULO 3

---
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -0,0 +1,37 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*1️⃣ EJERCICIO 01 1️⃣*/
+
+// En los siguientes ejercicios deberás reemplazar el valor null
+// por el correspondiente.
+
+// Crea una variable de tipo string.
+var nuevoString = "HOLA";
+console.log(nuevoString);
+
+// Crea una variable de tipo number.
+var nuevoNumero = 1;
+console.log(nuevoNumero);
+
+
+// Crea una variable de tipo boolean.
+var nuevoBoolean = true;
+console.log (nuevoBoolean);
+// Resuelve el siguiente problema matemático.
+
+var nuevaResta = 10 - 7 === 3;
+console.log (nuevaResta)
+// Resuelve el siguiente problema matemático.
+var nuevaMultiplicacion = 10 * 4 === 40;
+console.log (nuevaMultiplicacion)
+// Resuelve el siguiente problema matemático.
+var nuevoModulo = 21 % 5 === 1;
+console.log (nuevoModulo)
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   nuevoString,
+   nuevoNumero,
+   nuevoBoolean,
+   nuevaResta,
+   nuevaMultiplicacion,
+   nuevoModulo,
+};
---
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -0,0 +1,66 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*4️⃣ EJERCICIO 04 4️⃣*/
+
+// ⛔️ Recuerda que debes utilizar el objeto global "Math".
+
+function elevarAlCuadrado(num) {
+   // Retorna el valor de "num" elevado al cuadrado.
+   // Tu código:
+   var cuadrado = Math.pow (num, 2);
+   return cuadrado
+}
+
+var resultado = elevarAlCuadrado(2)
+console.log(resultado)
+
+function elevarAlCubo(num) {
+   // Retorna el valor de "num" elevado al cubo.
+   // Tu código:
+   return (num ** 3)
+}
+var resultado = elevarAlCubo (2)
+console.log(resultado)
+
+function elevar(num, exponent) {
+   // Retorna el valor de "num" elevado al exponente "exponent".
+   // Tu código:
+   return num ** exponent
+
+}
+var resultado = elevar (3,3);
+console.log(resultado)
+function redondearNumero(num) {
+   // Redondea "num" al entero más próximo y retórnalo.
+   // Tu código:
+return Math.round (num)
+}
+var resultado = redondearNumero (7.6)
+console.log(resultado)
+
+function redondearHaciaArriba(num) {
+   // Redondea "num" hacia arriba y retórnalo.
+   // Tu código:
+   return Math.ceil (num)
+}
+var resultado =redondearHaciaArriba (7.2);
+console.log (resultado);
+
+
+
+function numeroRandom() {
+   // Genera un número al azar entre 0 y 1 y retórnalo.
+   // Tu código:
+   
+return Math.random ()
+}
+var resultado = numeroRandom()
+console.log (resultado)
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   elevarAlCuadrado,
+   elevarAlCubo,
+   elevar,
+   redondearNumero,
+   redondearHaciaArriba,
+   numeroRandom,
+};
